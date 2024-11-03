31e0dc87bbe174f05f91991de26c9f05c90a6b90
Clemen Kok <clemen.kok@gmail.com>
2024-11-03 16:43:05 +0000

    forms beta

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+import Vueform from '@vueform/vueform'
+import vueformConfig from './vueform.config'
+import '@vueform/vueform/dist/vueform.css'
 
+const app = createApp(App)
+app.use(Vueform, vueformConfig)
 app.use(createPinia())
 app.use(router)
 app.mount('#app')
